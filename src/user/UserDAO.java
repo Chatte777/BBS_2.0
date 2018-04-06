b9b34c061c496b47532433e0762d666819ef25c7
Ma64bit <chsproud@gmail.com>
2018-04-06 18:21:47 +0900

    tmp commit

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -1,8 +1,6 @@
 package user;
 
-import java.net.InetAddress;	
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import dbConn.*;
